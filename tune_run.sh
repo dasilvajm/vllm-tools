364f0f239830e0afd69cac7bfdf733a29827b8af
dasilvajm <joe.dasilva@amd.com>
2024-09-23 10:41:58 -0400

    Update tune_run.sh

--- tune_run.sh
+++ tune_run.sh
@@ -11,4 +11,4 @@ modelfolder=/dockerx/$1
 
 echo $2, $3, $3
 
-VLLM_TUNE_FILE=/app/vllm/benchmarks/vllm-tools/gradlib_data/$1/gemms_tuned_$2_$3_$3.csv HIP_FORCE_DEV_KERNARG=1 VLLM_USE_TRITON_FLASH_ATTN=1 python /app/vllm/benchmark_latency.py --model $modelfolder/ --batch-size $2 --input-len $3 --output-len $3 --dtype float16 --trust-remote-code  --num-iters=10  --num-iters-warmup=1 
+VLLM_TUNE_FILE=/app/vllm/benchmarks/vllm-tools/gradlib_data/$1/gemms_tuned_$2_$3_$3.csv HIP_FORCE_DEV_KERNARG=1 VLLM_USE_TRITON_FLASH_ATTN=1 python /app/vllm/benchmarks/benchmark_latency.py --model $modelfolder/ --batch-size $2 --input-len $3 --output-len $3 --dtype float16 --trust-remote-code  --num-iters=10  --num-iters-warmup=1 
